de8cd0f25031a202fc285d26efc22f5d7a3388df
Andreas Stahlbauer <git@sthlbr.net>
2019-10-18 09:55:18 +0200

    Test adjusted

--- test/dummy.test.ts
+++ test/dummy.test.ts
@@ -1,4 +1,8 @@
-describe("GET /", () => {
-    it("should return 200 OK", (done) => {
+var assert = require('assert');
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
     });
 });
