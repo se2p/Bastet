0fe78a4074379c9633993bf1750a6f3fa9dbf00a
Andreas Stahlbauer <git@sthlbr.net>
2020-02-29 16:41:30 +0100

    Test runner script adjusted to be less verbose

--- test/programs/language-coverage/run-tests.sh
+++ test/programs/language-coverage/run-tests.sh
@@ -5,8 +5,60 @@ BASTET_ROOT="../../../"
 
 cd $BASTET_ROOT
 
+grep_statistic () {
+    CONTEXT=$1
+    IDENT=$2
+    cat $RESULT_FILE | grep $CONTEXT -A 5 | grep $IDENT | cut -d":" -f2 | cut -d"," -f1
+}
+
+parse_results () {
+    INPUT_FILE="$1"
+    RESULT_FILE="$2"
+    SATISFIED=$(grep_statistic "MultiPropertyAlgorithm" "num_satisfied")
+    VIOLATED=$(grep_statistic "MultiPropertyAlgorithm" "num_violated")
+    DURATION=$(grep_statistic "MultiPropertyAlgorithm" "duration")
+    DURATION=$(printf '%.*f\n' 2 $DURATION)
+
+    if [[ $INPUT_FILE == *_SAFE.sc ]]
+    then
+        EXPECTED_VIOLATED="0"
+        EXPECTED_SATISFIED="1"
+    else
+        EXPECTED_VIOLATED="1"
+        EXPECTED_SATISFIED="0"
+    fi
+
+    OK=1
+    if [ ! $EXPECTED_VIOLATED -eq "$VIOLATED" ]
+    then
+        OK=0
+    fi  
+
+    if [ ! $EXPECTED_SATISFIED -eq "$SATISFIED" ]
+    then
+        OK=0
+    fi  
+
+    printf "\t$DURATION\t$VIOLATED\t$SATISFIED"
+
+    if [ $OK -eq 1 ]
+    then
+        printf "\tOK"
+    else
+        printf "\tBUG"
+    fi    
+}
+
 for f in $(find $TEST_DIR -name "*.sc") 
 do
-    echo $f
-     ./scripts/bastet.sh -P $f -S test/programs/empty.sc -I src/public/intermediate.sc
+    RESULT_FILE=$(mktemp)
+    printf "`basename $f`"
+     ./scripts/bastet.sh \
+        -P $f \
+        -S test/programs/empty.sc \
+        -I src/public/intermediate.sc \
+        > $RESULT_FILE 2>&1 
+    parse_results $f $RESULT_FILE
+    rm $RESULT_FILE
+    printf "\n"
 done
