92d1c5dd79e11d8673aae27fea63e588604de16c
Andreas Stahlbauer <git@sthlbr.net>
2020-03-04 10:17:08 +0100

    Initial collection utils added

---
+++ src/bastet/utils/Collectionts.ts
@@ -0,0 +1,38 @@
+/*
+ *   BASTET Program Analysis and Verification Framework
+ *
+ *   Copyright 2019 by University of Passau (uni-passau.de)
+ *
+ *   Maintained by Andreas Stahlbauer (firstname@lastname.net),
+ *   see the file CONTRIBUTORS.md for the list of contributors.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+
+import {IllegalArgumentException} from "../core/exceptions/IllegalArgumentException";
+
+export function getTheOnlyElement<E>(it: Iterable<E>): E {
+   let result: E = null;
+
+   for (const r of it) {
+      if (result) {
+         throw new IllegalArgumentException("Iterable with more than one element");
+      } else {
+         result = r;
+      }
+   }
+
+   return result;
+}
