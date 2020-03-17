ae7329aec23118945cf2e9d8884462c54c4ed43f
Andreas Stahlbauer <git@sthlbr.net>
2020-03-17 06:38:09 +0100

    Printing the stack trace if available

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@
 import {Bastet} from "./bastet/Bastet";
 
 new Bastet().run().then(() => process.exit(0)).catch((e) => {
-    console.error("Running BASTET failed with " + e);
+    console.error("Running BASTET failed with: " + e.message);
+    if (e.stack) {
+        console.error(e.stack);
+    }
 });
 
