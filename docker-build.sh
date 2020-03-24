4c622504a3b2a8e1bdb683aeb8e344bc04c978ab
Andreas Stahlbauer <git@sthlbr.net>
2020-03-24 15:18:34 +0100

    No cache (docker build)

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,5 @@
 BASTET_VERSION=$(git rev-parse --short HEAD)
 TAG="bastet:$BASTET_VERSION"
 
-docker image build -f ./bastet.dockerfile -t $TAG .
+docker image build --no-cache -f ./bastet.dockerfile -t $TAG .
 
