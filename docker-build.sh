656af1fd67eb7cc0522f2454dc4c1a94289f16b6
Andreas Stahlbauer <git@sthlbr.net>
2020-03-24 14:41:11 +0100

    Typo fixed

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 BASTET_VERSION=$(git rev-parse --short HEAD)
 
-docker image build -f ./bastet.dockerfile -t bastet:$BASTE_VERSION .
+docker image build -f ./bastet.dockerfile -t bastet:$BASTET_VERSION .
 
