bff7ade64cf79b5834cc3bd2f306c18930f52e32
Andreas Stahlbauer <git@sthlbr.net>
2020-03-24 15:20:20 +0100

    Docker build script adjusted

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,5 @@
 BASTET_VERSION=$(git rev-parse --short HEAD)
 TAG="bastet:$BASTET_VERSION"
 
-docker image build --no-cache -f ./bastet.dockerfile -t $TAG .
+docker image build --pull --no-cache -f ./bastet.dockerfile --tag $TAG .
 
