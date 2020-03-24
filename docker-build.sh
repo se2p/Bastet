a55ac7300615e56543eb56af005d417966b4548e
Andreas Stahlbauer <git@sthlbr.net>
2020-03-24 14:47:57 +0100

    On the Docker image creation

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 BASTET_VERSION=$(git rev-parse --short HEAD)
+TAG="bastet:$BASTET_VERSION"
 
-docker image build -f ./bastet.dockerfile -t bastet:$BASTET_VERSION .
+docker image build -f ./bastet.dockerfile -t $TAG .
 
