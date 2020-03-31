8a7b7f8ac7c0e0e9bf19deb109c2df2199aad4e9
Andreas Stahlbauer <git@sthlbr.net>
2020-03-31 15:32:24 +0200

    Tets fixed

--- test/bastet/utils/smt/Z3.test.ts
+++ test/bastet/utils/smt/Z3.test.ts
@@ -66,12 +66,12 @@ test ("Implication. Unsat", () => {
     const x = new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("x"), NumberType.instance()));
     prover.push();
     const f = theories.boolTheory.and(
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(0))),
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))));
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(0))),
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))));
     prover.assert(f);
     const isUnsat: boolean = prover.isUnsat();
     expect(isUnsat).toBe(true);
@@ -82,12 +82,12 @@ test ("Implication. Sat", () => {
     const x = new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("x"), NumberType.instance()));
     prover.push();
     const f = theories.boolTheory.and(
-                theories.numTheory.isNumberEqualTo(
-                    theories.numTheory.abstractNumberValue(x),
-                    theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))),
-                theories.numTheory.isNumberEqualTo(
-                    theories.numTheory.abstractNumberValue(x),
-                    theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))));
+                theories.intTheory.isNumberEqualTo(
+                    theories.intTheory.abstractNumberValue(x),
+                    theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))),
+                theories.intTheory.isNumberEqualTo(
+                    theories.intTheory.abstractNumberValue(x),
+                    theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))));
     prover.assert(f);
     const isUnsat: boolean = prover.isUnsat();
     expect(isUnsat).toBe(false);
@@ -116,12 +116,12 @@ test("Lattice Include 4", () => {
     const lattice = new Z3FirstOrderLattice(theories.boolTheory, prover);
     const x = new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("x"), NumberType.instance()));
     const f = theories.boolTheory.and(
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))),
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))));
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))),
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))));
     const result = lattice.isIncluded(f, lattice.bottom());
     expect(result).toBe(false);
 });
@@ -130,31 +130,31 @@ test("Lattice Include 5", () => {
     const lattice = new Z3FirstOrderLattice(theories.boolTheory, prover);
     const x = new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("x"), NumberType.instance()));
     const f = theories.boolTheory.and(
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(0))),
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))));
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(0))),
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))));
     const result = lattice.isIncluded(f, lattice.bottom());
     expect(result).toBe(true);
 });
 
 test("Lattice Include T 1", () => {
     const lattice = new Z3FirstOrderLattice(theories.boolTheory, prover);
-    const t: Z3NumberFormula = theories.numTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("t"), NumberType.instance())));
-    const u1: Z3NumberFormula = theories.numTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u1"), NumberType.instance())));
-    const u2: Z3NumberFormula = theories.numTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u2"), NumberType.instance())));
+    const t: Z3NumberFormula = theories.intTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("t"), NumberType.instance())));
+    const u1: Z3NumberFormula = theories.intTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u1"), NumberType.instance())));
+    const u2: Z3NumberFormula = theories.intTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u2"), NumberType.instance())));
 
-    const u1_eq_1 = theories.numTheory.isNumberEqualTo(u1, theories.numTheory.fromConcreteNumber(new ConcreteNumber(1)));
-    const u2_eq_3 = theories.numTheory.isNumberEqualTo(u2, theories.numTheory.fromConcreteNumber(new ConcreteNumber(3)));
+    const u1_eq_1 = theories.intTheory.isNumberEqualTo(u1, theories.intTheory.fromConcreteNumber(new ConcreteNumber(1)));
+    const u2_eq_3 = theories.intTheory.isNumberEqualTo(u2, theories.intTheory.fromConcreteNumber(new ConcreteNumber(3)));
 
     const cond1 = theories.boolTheory.and(
         u1_eq_1,
-        theories.numTheory.isGreaterThan(t, u1));
+        theories.intTheory.isGreaterThan(t, u1));
     const cond2 = theories.boolTheory.and(
         u2_eq_3,
-        theories.numTheory.isGreaterThan(t, u2));
+        theories.intTheory.isGreaterThan(t, u2));
 
     const result = lattice.isIncluded(cond2, cond1);
     expect(result).toBe(false);
@@ -162,19 +162,19 @@ test("Lattice Include T 1", () => {
 
 test("Lattice Include T 2", () => {
     const lattice = new Z3FirstOrderLattice(theories.boolTheory, prover);
-    const t: Z3NumberFormula = theories.numTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("t"), NumberType.instance())));
-    const u1: Z3NumberFormula = theories.numTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u1"), NumberType.instance())));
-    const u2: Z3NumberFormula = theories.numTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u2"), NumberType.instance())));
+    const t: Z3NumberFormula = theories.intTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("t"), NumberType.instance())));
+    const u1: Z3NumberFormula = theories.intTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u1"), NumberType.instance())));
+    const u2: Z3NumberFormula = theories.intTheory.abstractNumberValue(new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("u2"), NumberType.instance())));
 
-    const u1_eq_1 = theories.numTheory.isNumberEqualTo(u1, theories.numTheory.fromConcreteNumber(new ConcreteNumber(1)));
-    const u2_eq_3 = theories.numTheory.isNumberEqualTo(u2, theories.numTheory.fromConcreteNumber(new ConcreteNumber(3)));
+    const u1_eq_1 = theories.intTheory.isNumberEqualTo(u1, theories.intTheory.fromConcreteNumber(new ConcreteNumber(1)));
+    const u2_eq_3 = theories.intTheory.isNumberEqualTo(u2, theories.intTheory.fromConcreteNumber(new ConcreteNumber(3)));
 
     const cond1 = theories.boolTheory.and(
         u1_eq_1,
-        theories.numTheory.isGreaterThan(t, u1));
+        theories.intTheory.isGreaterThan(t, u1));
     const cond2 = theories.boolTheory.and(
         u2_eq_3,
-        theories.numTheory.isGreaterThan(t, u2));
+        theories.intTheory.isGreaterThan(t, u2));
 
     const result = lattice.isIncluded(cond1, cond2);
     expect(result).toBe(false);
@@ -184,12 +184,12 @@ test("Lattice Join 1", () => {
     const lattice = new Z3FirstOrderLattice(theories.boolTheory, prover);
     const x = new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("x"), NumberType.instance()));
     const f1 = theories.boolTheory.and(
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))),
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))));
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))),
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))));
     const result = lattice.join(f1, lattice.bottom());
     prover.push();
     prover.assert(result);
@@ -201,12 +201,12 @@ test("Lattice Meet 1", () => {
     const lattice = new Z3FirstOrderLattice(theories.boolTheory, prover);
     const x = new VariableWithDataLocation(DataLocations.createTypedLocation(Identifier.of("x"), NumberType.instance()));
     const f1 = theories.boolTheory.and(
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))),
-        theories.numTheory.isNumberEqualTo(
-            theories.numTheory.abstractNumberValue(x),
-            theories.numTheory.fromConcreteNumber(new ConcreteNumber(42))));
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))),
+        theories.intTheory.isNumberEqualTo(
+            theories.intTheory.abstractNumberValue(x),
+            theories.intTheory.fromConcreteNumber(new ConcreteNumber(42))));
     const result = lattice.meet(f1, lattice.bottom());
     prover.push();
     prover.assert(result);
