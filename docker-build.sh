c672f962997a28e23194d7fbbce2d7e99385495e
Andreas Stahlbauer <stahlbauer@gmail.com>
2020-04-07 15:09:43 +0200

    Adjusted the docker-build script to work in the Infosun pool

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,10 @@
 BASTET_VERSION=$(git rev-parse --short HEAD)
 TAG="bastet:$BASTET_VERSION"
 
-docker image build --pull --no-cache -f ./bastet.dockerfile --tag $TAG .
+# Build the docker image
+dockerd-rootless-infosun --data-root /local/$USER/docker -- \
+    docker image build --pull --no-cache -f ./bastet.dockerfile --tag $TAG .
 
+# Export the docker image to a tar.bz2 file
+dockerd-rootless-infosun --data-root /local/$USER/docker -- \
+    docker save $TAG | bzip2 > bastet-docker-$BASTET_VERSION.tar.bz2
