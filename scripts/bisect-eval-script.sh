6ae32a7c760cf67ee0cfd5947bf7093f2bdae7cc
Andreas Stahlbauer <git@sthlbr.net>
2020-05-13 16:46:53 +0200

    Bisect scripts added

---
+++ scripts/bisect-eval-script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+PROGRAM="$1"
+MAX_MSECS="$2"
+
+# Root directory of BASTET
+cd $SCRIPT_DIR/..
+
+RESULT_FILE=$(mktemp)
+
+grep_statistic () {
+    CONTEXT="$1"
+    IDENT="$2"
+    cat $RESULT_FILE | grep $CONTEXT -A 5 | grep "$IDENT" | cut -d":" -f2 | cut -d"," -f1
+}
+
+npm install
+if ! npm run build-no-lint
+then
+    exit 125
+fi
+
+./scripts/bastet.sh \
+    -I src/public/library.sc \
+    -P $PROGRAM \
+    -S test/programs/empty.sc \
+    > $RESULT_FILE 2>&1
+
+DURATION=$(grep_statistic "MultiPropertyAlgorithm" "duration" | cut -d"." -f1)
+
+rm $RESULT_FILE
+
+if [ "$DURATION" -gt "$MAX_MSECS" ]
+then 
+    echo "Bug identified!"
+    exit 1
+else
+    echo "Seems fine."
+    exit 0
+fi
+
+
---
+++ scripts/bisect-performance-bug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+PROGRAM="$1"
+MAX_MSECS="$2"
+
+git bisect run $SCRIPT_DIR/bisect-eval-script.sh $PROGRAM $MAX_MSECS
+
+# Root directory of BASTET
+cd $SCRIPT_DIR/..
+
