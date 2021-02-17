119112de32c5bb9266ba366e2020317754f444c6
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2021-02-17 16:24:42 +0100

    Conversion of MockExtractors into Mocks

--- src/bastet/procedures/analyses/graph/witnesses/MockExtractor.ts
+++ src/bastet/procedures/analyses/graph/witnesses/MockExtractor.ts
@@ -28,15 +28,13 @@ import {CallStatement} from "../../../../syntax/ast/core/statements/CallStatemen
 import {ReturnStatement} from "../../../../syntax/ast/core/statements/ControlStatement";
 import {Preconditions} from "../../../../utils/Preconditions";
 import {ConcreteProgramState, ThreadState} from "../../control/ConcreteProgramState";
+import {AssignmentMock, Mock, ReturnValueMock} from "./ErrorWitness";
 
 /**
  * Can be applied to error paths to generate mocks of (probabilistic) functions. These mocks are useful in case one
  * wants to crate a replay-able error witness for a violation that Bastet found.
  */
 export interface MockExtractor {
-
-    /** The name of the LeiLa method this extractor should create a mock for. */
-    methodName(): string;
 
     /**
      * Processes the given transition label between two states of an error witness path. Note that a single label can
@@ -48,10 +46,14 @@ export interface MockExtractor {
      * @param successorState TODO
      */
     processOperations(transitionLabel: [ThreadState, ProgramOperation][], successorState: ConcreteProgramState): void;
+
+    getMock(): Mock
 }
 
 /** A mock extractor for the `randomBetween` block. */
 export class RandomIntegerMockExtractor implements MockExtractor {
+
+    private readonly _methodName: string = "randomBetween";
 
     /**
      * The values to be returned by the mock. Works similar to Mockito chain stubbing: the first call will return the
@@ -64,10 +66,6 @@ export class RandomIntegerMockExtractor implements MockExtractor {
      * error witness path.
      */
     private _methodEncountered: boolean = false;
-
-    methodName(): string {
-        return "randomBetween";
-    }
 
     processOperations(transitionLabel: [ThreadState, ProgramOperation][], successorState: ConcreteProgramState): void {
         /*
@@ -82,28 +80,34 @@ export class RandomIntegerMockExtractor implements MockExtractor {
             if (!this._methodEncountered && ast instanceof CallStatement) { // method to mock has been called
                 const callStmt = ast as CallStatement;
                 const methodName = callStmt.calledMethod.text;
-                if (this.methodName() === methodName) {
+                if (this._methodName === methodName) {
                     this._methodEncountered = true;
                 }
             } else if (this._methodEncountered && ast instanceof ReturnStatement) { // method to mock has returned
                 const returnStmt = ast as ReturnStatement;
                 Preconditions.checkState(returnStmt.resultVariable.isPresent(),
-                    `return value for ${this.methodName()} should have been present`);
+                    `return value for ${this._methodName} should have been present`);
                 const resultVariable = returnStmt.resultVariable.value();
 
                 const resultValue = successorState.getValueFor(resultVariable).value;
                 Preconditions.checkState(Number.isInteger(resultValue),
-                    `result of calling ${this.methodName()} should have been an integer`);
+                    `result of calling ${this._methodName} should have been an integer`);
                 this._returnValues.push(resultValue);
 
                 this._methodEncountered = false;
             }
         }
     }
+
+    getMock(): Mock {
+        return new ReturnValueMock<number>(this._methodName, this._returnValues);
+    }
 }
 
 /** A mock extractor for the `goToRandomPosition` block. */
 export class RandomPositionMockExtractor implements MockExtractor {
+
+    private readonly _methodName: string = "goToRandomPosition";
 
     /** The x- and y-coordinates returned by "goToRandomPosition", grouped by actor. */
     private readonly _coordinates: Map<string, {"x": number, "y": number}[]>
@@ -111,10 +115,6 @@ export class RandomPositionMockExtractor implements MockExtractor {
 
     /** Models a call stack for every thread (identified by its actor name). */
     private readonly _callStacks: Map<string, string[]> = new Map<string, string[]>();
-
-    methodName(): string {
-        return "goToRandomPosition";
-    }
 
     processOperations(operations: [ThreadState, ProgramOperation][], cp: ConcreteProgramState): void {
         for (const [threadState, operation] of operations) {
@@ -137,16 +137,16 @@ export class RandomPositionMockExtractor implements MockExtractor {
                 Preconditions.checkState(this._callStacks.has(actorName),
                     "there should have been a call statement prior to the return statement")
                 const methodName = this._callStacks.get(actorName).pop();
-                if (this.methodName() === methodName) {
+                if (this._methodName === methodName) {
                     // When "goToRandomPosition" is popped off the stack again we know that the new x- and y-coordinate
                     // are now readily available in the actor memory.
                     const xVal = cp.getActorMemory(actorName).getValue("x").value;
                     const yVal = cp.getActorMemory(actorName).getValue("y").value;
 
                     Preconditions.checkState(Number.isInteger(xVal),
-                        `x-coordinate of ${this.methodName} should have been a number`);
+                        `x-coordinate of ${this._methodName} should have been a number`);
                     Preconditions.checkState(Number.isInteger(yVal),
-                        `y-coordinate of ${this.methodName} should have been a number`);
+                        `y-coordinate of ${this._methodName} should have been a number`);
 
                     const coordinates = {"x": xVal, "y": yVal};
                     if (this._coordinates.has(actorName)) {
@@ -157,5 +157,13 @@ export class RandomPositionMockExtractor implements MockExtractor {
                 }
             }
         }
+    }
+
+    getMock(): Mock {
+        const assignments = [];
+        for (const [actorName, values] of this._coordinates.entries()) {
+            assignments.push({"actorName": actorName, "values": values, "index": 0})
+        }
+        return new AssignmentMock(this._methodName, assignments);
     }
 }
